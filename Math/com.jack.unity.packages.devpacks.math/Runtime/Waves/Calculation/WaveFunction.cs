2d7ffe8670119ff43bf0cd092c228a666a108890
Jack Cooper <jack.cooper7776@gmail.com>
2026-05-27 13:41:33 +1000

    Add Wave system to math package

---
+++ Math/com.jack.unity.packages.devpacks.math/Runtime/Waves/Calculation/WaveFunction.cs
@@ -0,0 +1,4 @@
+namespace Jack.Math.Waves.Calculation
+{
+    public delegate float WaveFunction(float time, float position, float phase, float amplitude, float frequency, float velocity, float bias);
+}
---
+++ Math/com.jack.unity.packages.devpacks.math/Runtime/Waves/Demo/WickFlicker.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Jack.Math.Waves.Demo
+{
+[ExecuteInEditMode]
+public class WickFlicker : MonoBehaviour
+{
+    [SerializeField] private Wave m_wave;
+    [SerializeField] private List<LightWave> m_lights;
+
+    private void Update()
+    {
+        Run(Time.time);
+    }
+
+    private void Run(float time)
+    {
+        foreach(LightWave lightWave in m_lights)
+        {
+            lightWave.Run(time);
+        }
+    }
+
+    public float Evaluate(float time, float position = 0)
+    {
+        float sum = 0;
+        foreach(LightWave lightWave in m_lights)
+        {
+            sum += lightWave.Evaluate(time, position);
+        }
+
+        return sum;
+    }
+
+    [Serializable]
+    private class LightWave
+    {
+        [SerializeField] private bool m_enabled = true;
+        [SerializeField] private Light m_light;
+
+        [SerializeField] private float m_brightnessOffset;
+        [SerializeField] private float m_timeOffset;
+
+        [SerializeField] private WaveCollection m_waves;
+
+        public WaveCollection Waves => m_waves;
+
+        public void Run(float time, float position = 0)
+        {
+            m_light.intensity = Evaluate(time + m_timeOffset, position) + m_brightnessOffset;
+        }
+
+        public float Evaluate(float time, float position = 0)
+        {
+            if(! m_enabled) { return 0; }
+
+            float value = Waves.Evaluate(time, position);
+            return value;
+        }
+    }
+}
+}
